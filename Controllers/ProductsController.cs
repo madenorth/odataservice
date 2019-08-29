1b0285909d51b13bba66436ce3ab1ec14b9a6846
Matt Williams <matt.williams@leeds.gov.uk>
2019-08-29 10:32:29 +0100

    initial

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.OData;
+using ODataService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ODataService.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        private List<Product> products = new List<Product>()
+        {
+            new Product()
+            {
+                ID = 1,
+                Name = "Bread",
+            }
+        };
+
+        [EnableQuery]
+        public List<Product> Get()
+        {
+            return products;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ODataService.Models;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ODataService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options =>
+            {
+                // https://blogs.msdn.microsoft.com/webdev/2018/08/27/asp-net-core-2-2-0-preview1-endpoint-routing/
+                // Because conflicts with ODataRouting as of this version
+                // could improve performance though
+                options.EnableEndpointRouting = false;
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddOData();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var builder = new ODataConventionModelBuilder(app.ApplicationServices);
+
+            builder.EntitySet<Product>("Products");
+
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+
+            app.UseHttpsRedirection();
+            app.UseMvc(routeBuilder =>
+            {
+                // and this line to enable OData query option, for example $filter
+                routeBuilder.Select().Expand().Filter().OrderBy().MaxTop(100).Count();
+
+                routeBuilder.MapODataServiceRoute("ODataRoute", "odata", builder.GetEdmModel());
+
+                // uncomment the following line to Work-around for #1175 in beta1
+                //routeBuilder.EnableDependencyInjection();
+            });
+            //app.UseMvc(); // Original
+        }
+    }
+}
